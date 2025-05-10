7711bf58eaaa28f764b84992e6ebd9dcd731b3d9
cosmintianu <cosmin.tianunicolae@yahoo.com>
2025-05-10 15:17:41 +0100

    Item entity features validation.

--- src/main/java/com/siemens/internship/Item.java
+++ src/main/java/com/siemens/internship/Item.java
@@ -1,10 +1,15 @@
 package com.siemens.internship;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +24,16 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    // Each item must have a name
+    @NotBlank(message = "Name cannot be blank")
     private String name;
     private String description;
     private String status;
 
-    // Add email regex validation
+    // Add email regex validation, the simple one also accepts name@domain, not name@domain.com
+    @Email(message = "Email is not valid", regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
 }
